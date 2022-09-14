f7ddd1248946f1ba06848a0ddb0fca658d560546
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-09-14 16:26:43 +0700

    Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    getCatalogs = CatalogItem.objects.all()
+    context = {
+        'name' : 'Shamira Alifanindya Prasetya',
+        'student_id' : '2106636376',
+        'katalog' : getCatalogs
+    }
+    return render(request, 'katalog.html', context)
